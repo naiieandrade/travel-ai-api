3f0ee652a872af3a51117db4d560803a396c43f4
naiieandrade <andradenaiara@hotmail.com>
2026-06-30 23:12:17 -0300

    add ollama model configs

--- app/core/config.py
+++ app/core/config.py
@@ -3,6 +3,8 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     API_KEY: str = "travel-api-key"
+    OLLAMA_MODEL: str = "llama3.2:1b"
+    USE_LLM: bool = True
 
     class Config:
         env_file = ".env"
